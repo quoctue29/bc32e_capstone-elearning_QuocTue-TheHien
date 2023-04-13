dc057649c1911b20f090a005ddb6ee95b91f4885
quoctue_ <87956563+quoctue29@users.noreply.github.com>
2023-04-13 12:00:21 +0700

    update

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
-const BaseURL = 'https://elearningnew.cybersoft.edu.vn'
-const TokenCybersoft = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAzMkUiLCJIZXRIYW5TdHJpbmciOiIxMS8wMy8yMDIzIiwiSGV0SGFuVGltZSI6IjE2Nzg0OTI4MDAwMDAiLCJuYmYiOjE2NTA0NzQwMDAsImV4cCI6MTY3ODY0MDQwMH0.nNcGn0C4SvUfrKPkxYBi5rhhLNuGbmfuND5eXehhzPQ'
-export const MaNhom = 'GP13'
-export const UserLogin = 'UserLogin'
-export const AccessToken = 'AccessToken'
+const BaseURL = "https://elearningnew.cybersoft.edu.vn";
+const TokenCybersoft =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOb2RlanMgMjkiLCJIZXRIYW5TdHJpbmciOiIxMi8wOS8yMDIzIiwiSGV0SGFuVGltZSI6IjE2OTQ0NzY4MDAwMDAiLCJuYmYiOjE2Nzc2MDM2MDAsImV4cCI6MTY5NDYyNDQwMH0._Rqf64wjEFiYvyTO1ebVw_Q5phbKYLalDcLoDn-rv0U";
+export const MaNhom = "GP13";
+export const UserLogin = "UserLogin";
+export const AccessToken = "AccessToken";
 
-
-export const api = axios.create()
-api.interceptors.request.use(config => {
-   return {
-      ...config,
-      baseURL: BaseURL,
-      headers: {
-         TokenCybersoft: TokenCybersoft,
-         Authorization: 'Bearer ' + JSON.parse(localStorage.getItem(AccessToken))
-      },
-   }
-})
+export const api = axios.create();
+api.interceptors.request.use((config) => {
+  return {
+    ...config,
+    baseURL: BaseURL,
+    headers: {
+      TokenCybersoft: TokenCybersoft,
+      Authorization: "Bearer " + JSON.parse(localStorage.getItem(AccessToken)),
+    },
+  };
+});
